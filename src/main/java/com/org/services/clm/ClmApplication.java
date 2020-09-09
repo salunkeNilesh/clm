fc922b295266e704979ee0fa5f1fff4e520affd3
unknown <nilesh.salunke@briozing.com>
2020-09-09 18:05:40 +0530

    Signed-off-by: unknown <nilesh.salunke@briozing.com>

--- src/main/java/com/org/services/clm/ClmApplication.java
+++ src/main/java/com/org/services/clm/ClmApplication.java
@@ -3,6 +3,7 @@ package com.org.services.clm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,8 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("com.org")
 @EnableJpaRepositories("com.org")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClmApplication.class, args);
 	}
